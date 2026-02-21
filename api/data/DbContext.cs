df5d2ed495db8d127be9191f1137db489b9b5d35
ifuji <ifujihaku1216@gmail.com>
2026-02-21 22:34:05 +0900

    一様コミットまだエラーがある

---
+++ api/data/DbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Api.Models;
+
+namespace Api.Data;
+
+public class AppDbContext : DbContext
+{
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+  public DbSet<User> Users => Set<User>();
+  public DbSet<Post> Posts => Set<Post>();
+  public DbSet<Comment> Comments => Set<Comment>();
+  public DbSet<Tree> Trees => Set<Tree>();
+  public DbSet<Topic> Topics => Set<Topic>();
+  public DbSet<Tag> Tags => Set<Tag>();
+  public DbSet<PostTag> PostTags => Set<PostTag>();
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+  }
+}
