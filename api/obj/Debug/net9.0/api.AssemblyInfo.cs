f929f46174f3aecdabb26370ecbac1d7ee3e428a
ifuji <ifujihaku1216@gmail.com>
2026-02-23 01:36:59 +0900

    不要なデータベースファイルの削除とAssembly情報の更新

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c398a1e5fafc3989929925f3ab504a59e5f0cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18120fd56f6f01d565450abda8c7694e6fdff18")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
