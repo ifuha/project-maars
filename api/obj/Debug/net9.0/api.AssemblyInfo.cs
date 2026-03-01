5cb8ac9108b4efca398a4b55e98a84b128bac5f7
ifuji <ifujihaku1216@gmail.com>
2026-03-01 17:55:06 +0900

    コミット

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b43d8b6b8f02070050ba592d3f472e9b61c39638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728acdb5ff3c016428aba4f1a70f331fac86e2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
