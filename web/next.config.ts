ae154435dfcb02ab5766765668edabe7cdc910a3
ifuji <ifujihaku1216@gmail.com>
2026-03-01 18:49:23 +0900

    変更

--- web/next.config.ts
+++ web/next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `https://project-mars-production.up.railway.app/api/:path*`,
       },
     ];
   },
